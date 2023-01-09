c691ab5d010e59ef2f0ad9d67d9c64d006c72932
literallymarvellous <ahiaraikechukwu@gmail.com>
2023-01-09 08:01:19 +0100

    added head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Linktree</title>
+      <meta name="description" content="Nextjs + tailwind" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
